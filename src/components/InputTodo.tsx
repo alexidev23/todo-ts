e5ba2fcc4b74e05814daa4d4ed06b4021726eb80
alexidev23 <alexisescobar1234@gmail.com>
2025-06-29 15:26:19 -0300

    añadir vista mobile

--- src/components/InputTodo.tsx
+++ src/components/InputTodo.tsx
@@ -3,7 +3,7 @@ import { Input } from "./ui/input";
 export function InputTodo() {
   const todo = false
   return (
-    <div className="w-[600px] mx-auto mt-10">
+    <div className="w-full px-10 md:px-0 md:w-[600px] mx-auto mt-10">
       <Input 
         type="text"
         placeholder="new todo..."
