584af87b0d4e3868ef414befde7eeb6e53f37a79
alexisDev23 <alexisescobar1234@gmail.com>
2025-10-30 18:58:20 -0300

    mejorando algunos tests

--- cypress/e2e/todo.cy.ts
+++ cypress/e2e/todo.cy.ts
@@ -5,17 +5,20 @@ const addTodos = (todos: string[]) => {
   });
 };
 
+const Todos = ["Mi primera tarea", "Mi segunda tarea", "Mi tercera tarea", "Mi cuarta tarea"]
 
 // Creamos un archivo de prueba básico para nuestro proyecto Todo en Cypress
 describe('TO-DO app - tests', () => {
   beforeEach(() => {
     // Visitar la aplicación antes de cada test
     cy.visit('https://todo-ts-three-kappa.vercel.app/');
-    addTodos(["Mi primera tarea", "Mi segunda tarea", "Mi tercera tarea", "Mi cuarta tarea"]);
+    addTodos(Todos);
   });
 
-  it("Agrega tareas", () => {
-    addTodos(["Mi tercera tarea", "Mi cuarta tarea"]);
+  it("Verificar que las tareas se agregan correctamente", () => {
+    Todos.forEach(todo => {
+      cy.contains(todo).should("exist");
+    });
   });
 
   it("Marca una tarea como completada", () => {
@@ -27,26 +30,31 @@ describe('TO-DO app - tests', () => {
 
   it("Borra una tarea", () => {
     cy.contains("Mi segunda tarea").parent().find('button[data-cy="delete-btn"]').click();
+    cy.contains("Mi cuarta tarea").parent().find('button[data-cy="delete-btn"]').click();
 
-    cy.get('div').contains("items").should('contain.text', '3 items');
+    cy.get('div').contains("items").should('contain.text', `${Todos.length - 2} items`);
   });
 
-  it("Edita una tarea", () => {
+  it.only("Edita una tarea", () => {
     // Hacer clic en el botón editar
     cy.contains("Mi segunda tarea")
       .parent()
-      .find('button[data-cy="edit-btn"]')
-      .click()
-      .get("form input[value='Mi segunda tarea']")
+      .find('[data-cy="edit-btn"]')
+      .click();
+
+    // Esperar a que aparezca el input de edición
+    cy.get("input[data-cy='edit-input']")
+      .should("be.visible") // ✅ Espera explícita
       .clear()
-      .type("Segunda tarea editada{enter}"); 
+      .type("Segunda tarea editada{enter}");
 
     // Verificar que el texto se actualizó
     cy.contains("Segunda tarea editada").should("exist");
+    cy.contains("Mi segunda tarea").should("not.exist");
   });
 
   it("Verificar contador de tareas sin completar", () => { 
-    cy.contains("items").should('contain.text', '4 items');
+    cy.contains("items").should('contain.text', `${Todos.length} items`);
   });
 
   it("Filtra tareas Active y Completed", () => {
@@ -65,7 +73,7 @@ describe('TO-DO app - tests', () => {
     cy.contains("Mi segunda tarea");
   });
 
-  it("Persistencia en localStorage tras recargar", () => {
+  it.only("Persistencia en localStorage tras recargar", () => {
     cy.contains("Mi segunda tarea").parent().find('button[role="checkbox"]').click();
     cy.reload();
 
