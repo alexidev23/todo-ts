fbb51736a5607cf3acaad0ad3d0310cbe4ef937e
alexisDev23 <alexisescobar1234@gmail.com>
2025-10-30 11:04:37 -0300

    refactor: agregar propiedades al componente FilterTasks para mostrar el total de tareas sin completar

--- src/components/FilterTasks.tsx
+++ src/components/FilterTasks.tsx
@@ -1,8 +1,15 @@
-export function FilterTasks() {
+interface FilterTasksProps {
+  total: number;
+}
+  // completed: number;
+  // onFilterChange: (filter: "all" | "active" | "completed") => void;
+
+
+export function FilterTasks({ total }: FilterTasksProps) {
   return (
     <div className="w-full flex items-center justify-between px-1 py-4">
       <div className="px-1.5 py-1 rounded-md text-sm text-slate-950 dark:text-gray-500">
-        0 items
+        {total} items
       </div>
       <div className="flex items-center gap-2">
         <button className="px-1.5 py-0.5 rounded-md text-sm border border-slate-950 dark:border-slate-500 font-semibold cursor-pointer text-slate-950 dark:text-gray-500 hover:bg-blue-600 hover:text-slate-300 hover:border-slate-300 hover:dark:text-white hover:dark:border-white">
