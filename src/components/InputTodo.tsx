4eb3e829d23281842b3fadc3fd30c5cc5b50d74b
alexisDev23 <alexisescobar1234@gmail.com>
2025-10-30 11:04:15 -0300

    refactor: agregar manejo de estado y funcionalidad de envío en el componente

--- src/components/InputTodo.tsx
+++ src/components/InputTodo.tsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import { Input } from "./ui/input";
 
-export function InputTodo() {
+interface InputTodoProps {
+  addTodo: (task: string) => void;
+}
+
+export function InputTodo({ addTodo }: InputTodoProps) {
+  const [newTodo, setNewTodo] = useState("");
+  
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    if (!newTodo.trim()) return;
+    addTodo(newTodo.trim());
+    setNewTodo("");
+  };
+
   return (
-    <div className="w-full px-10 md:px-0 md:w-[600px] mx-auto mt-10">
+    <form onSubmit={handleSubmit} className="w-full px-10 md:px-0 md:w-[600px] mx-auto mt-10">
       <Input 
         type="text"
         placeholder="new todo..."
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
         className="focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:border-transparent rounded-md shadow-sm w-full p-3 text-xl mb-6"
       />
-    </div>
+    </form>
   )
 }
